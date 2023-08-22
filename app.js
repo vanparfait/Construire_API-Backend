bfb5f7b70536dd2777e39c9b0e0cb71248453ae2
vanparfait <nguemechiaparfait@gmail.com>
2023-08-22 12:12:16 +0100

    OPTIMISATIONS DE LA STRUCTURE DU CODE DE NOTRE API

--- app.js
+++ app.js
@@ -34,48 +34,10 @@ const bcrypt = require("bcrypt");
 
 const jwt = require("jsonwebtoken");
 
-app.post("/api/auth/signup", (req, res, next) => {
-  bcrypt
-    .hash(req.body.password, 10)
-    .then((hash) => {
-      const user = new User({
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then(() =>
-          res.status(201).json({ message: "Utilisateur créé avec succès !" })
-        )
-        .catch((error) => res.status(400).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
-});
+//: Importation du routeur User
+const userRoutes = require("./routes/user");
 
-app.post("/api/auth/login", (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      console.log(user);
-      if (!user) {
-        return res.status(401).json({ error: "L'utilisateur n'existe pas ! " });
-      }
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res.status(401).json({ error: "Mot de passe incorrect !" });
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
-              expiresIn: "24h",
-            }),
-          });
-        })
-        .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
-});
+app.use("/api/auth", userRoutes);
 
 app.use((req, res, next) => {
   console.log("Requête est reçue !");
