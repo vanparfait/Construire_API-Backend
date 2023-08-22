cf85c2ca82804ae88f276fdbe306b8eb1e9ef5cb
vanparfait <nguemechiaparfait@gmail.com>
2023-08-22 08:17:02 +0100

    CONFIGURATION DE LA BASE DE DONNÉES MONGODB

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://danielle:danielle2023@cluster0.xybqyya.mongodb.net/DANIELLE",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use((req, res, next) => {
   console.log("Requête est reçue !");
