77413ce88f7e96842e255c738f4fd52c152e6eee
vanparfait <nguemechiaparfait@gmail.com>
2023-08-22 09:19:15 +0100

    Base de données MongoDB avec enregistrement d'un utilisateur depuis le frontend

--- app.js
+++ app.js
@@ -5,11 +5,50 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://danielle:danielle2023@cluster0.xybqyya.mongodb.net/DANIELLE",
+    "mongodb+srv://danielle:danielle2023@cluster0.xybqyya.mongodb.net/FOKOU?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const User = require("./models/User");
+
+const bcrypt = require("bcrypt");
+
+app.post("/api/auth/signup", (req, res, next) => {
+  bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user
+        .save()
+        .then(() =>
+          res.status(201).json({ message: "Utilisateur créé avec succès !" })
+        )
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+});
 
 app.use((req, res, next) => {
   console.log("Requête est reçue !");
