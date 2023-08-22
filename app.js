beb829815607831bdea8f0dc014b91beb493afcc
vanparfait <nguemechiaparfait@gmail.com>
2023-08-22 11:10:51 +0100

    Création de tokens d’authentification

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ const User = require("./models/User");
 
 const bcrypt = require("bcrypt");
 
+const jwt = require("jsonwebtoken");
+
 app.post("/api/auth/signup", (req, res, next) => {
   bcrypt
     .hash(req.body.password, 10)
@@ -46,6 +48,31 @@ app.post("/api/auth/signup", (req, res, next) => {
           res.status(201).json({ message: "Utilisateur créé avec succès !" })
         )
         .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+});
+
+app.post("/api/auth/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      console.log(user);
+      if (!user) {
+        return res.status(401).json({ error: "L'utilisateur n'existe pas ! " });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: "Mot de passe incorrect !" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ userId: user._id }, "RANDOM_TOKEN_SECRET", {
+              expiresIn: "24h",
+            }),
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
 });
